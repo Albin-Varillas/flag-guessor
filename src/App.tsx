6e67599660a76b7becf4585020db48a36c85ba08
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-28 23:58:18 +0100

    added guess function and contient selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import type { Country } from "./types/Country"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [countries, setCountries] = useState<Country[]>([])
+    const [country, setCountry] = useState<Country | null>(null)
+    const [input, setInput] = useState("")
+    const [isWinner, setIsWinner] = useState(false)
+    const continents = ["europe", "asia", "africa"] as const
+    const [selectedContinent, setSelectedContinent] =
+        useState<typeof continents[number]>("europe")
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    useEffect(() => {
+        async function getCountries() {
+            const res = await fetch(
+                "https://restcountries.com/v3.1/region/" + selectedContinent
+            )
+            const data = await res.json()
+            setCountries(data)
+        }
+        getCountries()
+    }, [selectedContinent])
+
+    useEffect(() => {
+        getRandomCountry()
+    }, [countries])
+
+    function getRandomCountry() {
+        const maxlength = countries.length
+        const randomIndex = Math.floor(Math.random() * maxlength)
+        setCountry(countries[randomIndex])
+    }
+
+    function getHint() {
+        setInput(country?.name.common.substring(0, 3) || "")
+    }
+
+    function guessCountry() {
+        if (input.toLowerCase() === country?.name.common.toLowerCase()) {
+            getRandomCountry()
+            setInput("")
+            setIsWinner(true)
+        } else {
+            setIsWinner(false)
+        }
+    }
+
+    return (
+        <div className="App">
+            {selectedContinent}
+            <div>
+                {continents.map((continent) => (
+                    <button
+                        onClick={() => {
+                            setSelectedContinent(continent)
+                        }}
+                    >
+                        {continent}
+                    </button>
+                ))}
+            </div>
+            {country && <img src={country.flags.png} alt="flag-png" />}
+            <div>
+                <input
+                    type="text"
+                    placeholder="guess country..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button onClick={() => guessCountry()}>Guess country</button>
+                <button onClick={() => getHint()}>Get hint</button>
+                <p>
+                    {isWinner
+                        ? "Wow, du är smart"
+                        : "Oj, plugga lite är du snäll..."}
+                </p>
+            </div>
+            <button onClick={() => getRandomCountry()}>New Country</button>
+        </div>
+    )
 }
 
 export default App
